3edfb448541d7e0cecf40cf89bcaa268a55b2bdc
pavel8293-pavel <pavel8293@gmail.com>
2020-11-01 18:50:18 +0200

    feat: add hanoi-tower and transform array solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+  let turn = Math.pow(2,disksNumber) - 1
+
+  let transferPerSec = turnsSpeed/3600
+
+  let second = turn/transferPerSec 
+  let a = new Object();
+  a  = {     
+    turns: turn, seconds: Math.trunc(second )     
+  };
+  return a
+    };
+
+
+  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =function transform(array) {
+    if(array === null || array === undefined || typeof array !== 'object'){
+        throw new Error('THROWN')
+    }else{
+        let tempArray = []
+        for (let i = 0; i < array.length; i++){
+            if(array[i] !== '--discard-next' && array[i] !== '--discard-prev' && array[i] !== '--double-next' && array[i] !== '--double-prev'){
+                tempArray.push(array[i])
+            }else{
+                if(array[i] === '--discard-next'){
+                    if(array[-1] === '--discard-next'){
+                        tempArray=tempArray
+                    }else{
+                        tempArray.push('')
+                        i+=2
+                    }
+                }else if(array[i] === '--discard-prev'){
+                    if(array[0] === '--discard-prev'){
+                        tempArray=tempArray
+                    }else{
+                        tempArray.pop()
+                        tempArray.push('')
+                    }
+                }else if(array[i] === '--double-next'){
+                    if(array[-1] === '--double-next'){
+                        tempArray = tempArray
+                    }else{                    
+                        tempArray.push(array[i + 1])}
+                }else if(array[i] === '--double-prev'){
+                    if(array[0] === '--double-prev'){
+                        tempArray = tempArray
+                    }else{tempArray.push(array[i-1])}        
+                }
+            }   
+        }
+ let arr = tempArray.filter(e => e)
+ return arr
+
+    }
+  };
